80bd37e7ba0792d584fa1ac42c9b00433290d966
masudrana <masudranaacb@gmail.com>
2022-01-29 15:05:31 +0600

    object diclaration

---
+++ object.js
@@ -0,0 +1,13 @@
+// object disclration
+var student={
+    name:'masud rana',
+    id:1125,
+    rowls:77,
+    department:'scince',
+}
+
+var book={
+    title:'guopraphy is the best book',
+    author:"muna",
+    publicatin:'drible',
+}
